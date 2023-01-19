65965c90f860a8e0844e5dcb610b856dd2a41855
Chelsea Burr <cburr.dev@gmail.com>
2023-01-19 14:11:28 -0500

    Added range and fixed pagination 3 pages and under

--- src/hooks/usePagination.js
+++ src/hooks/usePagination.js
@@ -1,11 +1,12 @@
 import { useMemo } from 'react'
 
 export const DOTS = '...'
+const range = (start, end) => Array.from(Array(end - start + 1), (_, index) => index + start)
 
 function usePagination({ 
     currentPage, 
     totalCount, 
-    pageSize 
+    pageSize, 
 }) {
     /*
 
@@ -18,18 +19,19 @@ function usePagination({
     const paginationRange = useMemo(() => {
         const totalPages = Math.ceil(totalCount / pageSize)
 
-        if (totalCount === pageSize) return [1]
-        if (totalPages === 3) return [1, 2, 3]
-        if (currentPage <= 2) return [1, 2, 3, DOTS, totalPages]
-        if (currentPage >= totalPages - 1)
-            return [1, DOTS, totalPages - 2, totalPages - 1, totalPages]
+        if (totalPages <= 3) 
+            return [...range(1, totalPages)]
+
+        if (currentPage <= 2) 
+            return [...range(1, 3), DOTS, totalPages]
+
+        if (currentPage >= totalPages - 1) 
+            return [1, DOTS, ...range(totalPages - 2, totalPages)]
 
         return [
             1,
             DOTS,
-            currentPage - 1,
-            currentPage,
-            currentPage + 1,
+            ...range(currentPage - 1, currentPage + 1),
             DOTS,
             totalPages,
         ]
